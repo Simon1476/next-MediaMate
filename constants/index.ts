c1b22f468246796d691ca6e6e6fe455669c9729f
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-08-03 23:56:43 +0900

    feat: 초기 프로젝트 설치 및 TMDB session ID 생성 구현
    * chore: 사용자 정의 텍스트 유틸리티 추가
    
    * feat: Navbar 상수 constants.ts 파일 추가

---
+++ constants/index.ts
@@ -0,0 +1,22 @@
+export const navbarLinks = [
+  {
+    imgUrl: "/icons/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgUrl: "/icons/movie.svg",
+    route: "/movies",
+    label: "Movie",
+  },
+  {
+    imgUrl: "/icons/late.svg",
+    route: "/top-rated",
+    label: "Top Rated",
+  },
+  {
+    imgUrl: "/icons/latest.svg",
+    route: "/latest",
+    label: "Latest",
+  },
+];
