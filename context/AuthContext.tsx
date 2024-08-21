a4cf85d046f1e2704c1fad0ee600696b8c7cb47d
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-08-21 23:51:16 +0900

    refactor: AuthProvider 리팩토링: 사용자 정보 가져오기 및 상태 동기화 개선

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -6,23 +6,39 @@ import { createContext, useContext, useEffect, useState } from "react";
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
+const USERNAME_KEY = "username";
+const ACCOUNT_ID_KEY = "accountId";
+
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [username, setUserName] = useState<string | null>(null);
+  const [accountId, setAccountId] = useState<string | null>(null);
 
   useEffect(() => {
-    const storedUsername = localStorage.getItem("username");
-    if (storedUsername) {
-      setUserName(storedUsername);
-    }
+    // 브라우저 환경에서만 실행되도록 보장
+    const storedUsername = localStorage.getItem(USERNAME_KEY);
+    const storedAccountId = localStorage.getItem(ACCOUNT_ID_KEY);
+
+    if (storedUsername) setUserName(storedUsername);
+    if (storedAccountId) setAccountId(storedAccountId);
   }, []);
 
   useEffect(() => {
+    // username 상태가 변경될 때 로컬 스토리지 업데이트
     if (username) {
-      localStorage.setItem("username", username);
+      localStorage.setItem(USERNAME_KEY, username);
     } else {
-      localStorage.removeItem("username");
+      localStorage.removeItem(USERNAME_KEY);
     }
   }, [username]);
+
+  useEffect(() => {
+    // accountId 상태가 변경될 때 로컬 스토리지 업데이트
+    if (accountId) {
+      localStorage.setItem(ACCOUNT_ID_KEY, accountId);
+    } else {
+      localStorage.removeItem(ACCOUNT_ID_KEY);
+    }
+  }, [accountId]);
 
   const fetchUserInfo = async () => {
     try {
@@ -31,6 +47,9 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
       if (response.ok) {
         const data = await response.json();
         setUserName(data.username);
+        setAccountId(data.id);
+      } else {
+        console.error("Failed to fetch user info:", response.statusText);
       }
     } catch (error) {
       console.error("Error fetching user info:", error);
@@ -39,11 +58,15 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 
   const signOut = () => {
     setUserName(null);
+    setAccountId(null);
     deleteSessionId();
-    localStorage.removeItem("username");
+    localStorage.removeItem(USERNAME_KEY);
+    localStorage.removeItem(ACCOUNT_ID_KEY);
   };
   return (
-    <AuthContext.Provider value={{ username, fetchUserInfo, signOut }}>
+    <AuthContext.Provider
+      value={{ username, fetchUserInfo, signOut, accountId }}
+    >
       {children}
     </AuthContext.Provider>
   );
