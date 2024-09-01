12759730211bb73e60a20519aa01daa7962e047a
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-09-01 23:10:27 +0900

    feat: TV 쇼 카드에 링크 추가 및 평점과 방영 날짜 표시 개선
    - TV 쇼 카드에 링크를 추가하여 TV 쇼 상세 페이지로 이동
    
    - 평점이 0일 때 "No"라는 텍스트를 표시하도록 변경
    
    - TV 쇼의 첫 방영 날짜(`first_air_date`)를 추가하여 카드에 표시

--- components/tv/TvShowCard.tsx
+++ components/tv/TvShowCard.tsx
@@ -1,6 +1,7 @@
 import { TMDBTVShow } from "@/types/tmdb";
 
 import Image from "next/image";
+import Link from "next/link";
 
 const TvShowCard = ({ tvShow }: { tvShow: TMDBTVShow }) => {
   const formattedVoteAverage =
@@ -9,20 +10,34 @@ const TvShowCard = ({ tvShow }: { tvShow: TMDBTVShow }) => {
       ? Math.floor(tvShow.vote_average)
       : parseFloat(tvShow.vote_average.toFixed(1));
 
+  const tvShowPoster = tvShow.poster_path
+    ? `https://image.tmdb.org/t/p/w300${tvShow.poster_path}`
+    : "/image/no-poster1.png";
+
   return (
-    <div className="flex flex-col w-[230px] h-[500px] rounded-md overflow-hidden">
-      <Image
-        src={`https://image.tmdb.org/t/p/w220_and_h330_face${tvShow.poster_path}`}
-        alt={tvShow.name}
-        width={220}
-        height={330}
-        className="w-full"
-      />
-      <div className="space-y-4 border-gray-100 pt-6 pr-3 pb-3">
-        <span className="font-extrabold text-red-500">
-          {formattedVoteAverage} Rate
-        </span>
-        <h2 className="text-white text-lg font-bold truncate">{tvShow.name}</h2>
+    <div className="flex flex-col overflow-hidden rounded-md">
+      <div>
+        <Link href={`/tv/${tvShow.id}`}>
+          <Image
+            src={tvShowPoster}
+            alt={tvShow.name}
+            width={220}
+            height={330}
+            className="w-full"
+          />
+        </Link>
+      </div>
+
+      <div className="pt-6 pb-3 pr-3 space-y-4 border-gray-100">
+        <div className="flex items-center justify-between">
+          <span className="font-extrabold text-red-500">
+            {formattedVoteAverage === 0 ? "No" : formattedVoteAverage} Rate
+          </span>
+          <span className="ml-4 font-extrabold text-red-500">
+            {tvShow.first_air_date}
+          </span>
+        </div>
+        <h2 className="text-lg font-bold text-white truncate">{tvShow.name}</h2>
         <p className="text-sm text-gray-200 truncate">{tvShow.overview}</p>
       </div>
     </div>
