8c549e4a853f8550f580c487ae3f7bac5ba243d2
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-09-01 23:24:18 +0900

    feat: TvShowDetail 페이지 레이아웃을 반응형으로 변경 GenresList 추가
    - TvShowDetail 컴포넌트의 레이아웃을 반응형으로 변경하여 모바일과 데스크탑에서 적절히 표시되도록 수정
    
    - 상세 정보 섹션에 `GenresList` 컴포넌트를 추가하여 장르 정보를 표시
    
    - `getSessionId` 함수를 사용하여 로그인 상태에 따라 좋아요 버튼의 표시 여부를 결정

--- app/(root)/tv/[id]/page.tsx
+++ app/(root)/tv/[id]/page.tsx
@@ -1,4 +1,6 @@
 import LikeButton from "@/components/buttons/LikeButton";
+import GenresList from "@/components/genre/GenresList";
+import { getSessionId } from "@/lib/cookies";
 import { getTvshowDetail } from "@/lib/tmdb";
 import { formatRuntime } from "@/lib/utils";
 import Image from "next/image";
@@ -9,11 +11,10 @@ const TvShowDetail = async ({ params }: { params: { id: string } }) => {
   const tvShowDetail = await getTvshowDetail(id);
   const genres = tvShowDetail.genres.map((genre) => genre);
 
+  const sessionId = await getSessionId();
   return (
-    <div
-      className={`bg-cover bg-no-repeat bg-[url('https://image.tmdb.org/t/p/w1280${tvShowDetail.backdrop_path}')]'`}
-    >
-      <div className={`flex gap-16`}>
+    <div className="flex flex-col gap-16 md:flex-row">
+      <div className="self-center flex-grow md:flex-none">
         <Image
           src={`https://media.themoviedb.org/t/p/w300_and_h450_bestv2${tvShowDetail.poster_path}`}
           alt={`${
@@ -22,75 +23,49 @@ const TvShowDetail = async ({ params }: { params: { id: string } }) => {
           width={300}
           height={450}
         />
-        {/* 상세 정보 */}
-        <div className="flex flex-col gap-4">
-          {/* 제목*/}
-          <h2 className="text-32 text-white font-bold">{tvShowDetail.name}</h2>
+      </div>
+      {/* 상세 정보 */}
+      <div className="flex flex-col items-center gap-4 md:items-stretch">
+        {/* 제목*/}
+        <h2 className="font-bold text-white text-32">{tvShowDetail.name}</h2>
 
-          <p className="flex items-center text-white gap-2">
-            <Image
-              src="/icons/gold-star.svg"
-              alt="Star icon"
-              width={20}
-              height={20}
-            />
-            {/* 평점 */}
-            <span className="text-20 font-semibold">
-              {tvShowDetail.vote_average}
-            </span>
-            <span className="text-20">({tvShowDetail.vote_count})</span>
+        <p className="flex items-center gap-2 text-white">
+          <Image
+            src="/icons/gold-star.svg"
+            alt="Star icon"
+            width={20}
+            height={20}
+          />
+          {/* 평점 */}
+          <span className="font-semibold text-20">
+            {tvShowDetail.vote_average}
+          </span>
+          <span className="text-20">({tvShowDetail.vote_count})</span>
 
-            {tvShowDetail.episode_run_time.length > 0 && (
-              <>
-                <span>•</span>
-                <span>{formatRuntime(tvShowDetail.episode_run_time[0])}</span>
-              </>
-            )}
-          </p>
+          {tvShowDetail.episode_run_time.length > 0 && (
+            <>
+              <span>•</span>
+              <span>{formatRuntime(tvShowDetail.episode_run_time[0])}</span>
+            </>
+          )}
+        </p>
 
-          <p className="text-white">{tvShowDetail.overview}</p>
+        <p className="text-white">{tvShowDetail.overview}</p>
 
-          <div className="flex gap-2">
-            {/* 장르 */}
-            <span className="text-white font-medium min-w-24">장르</span>
-            {genres.map((genre) => (
-              <span className="text-white" key={genre.id}>
-                {genre.name}
-              </span>
-            ))}
-          </div>
-          {/* 좋아요, 시청 목록, 평가 버튼 */}
+        <div className="flex flex-wrap justify-center gap-2 md:flex-nowrap md:justify-normal">
+          {/* 장르 */}
+          <span className="font-medium text-center text-white min-w-24 md:text-left">
+            장르
+          </span>
+          <GenresList genres={genres} mediaType="tv" />
+        </div>
+        {/* 좋아요, 시청 목록, 평가 버튼 */}
+        {sessionId && (
           <div className="flex">
             <LikeButton mediaType="tv" mediaId={id} />
           </div>
-        </div>
+        )}
       </div>
-      {/* 출연자 목록*/}
-      {/* <div>
-        <h3 className="text-20 text-white ">액션 배우</h3>
-        <ol className="overflow-x-scroll flex gap-3">
-          {movieActionPlayers.map((player) => (
-            <li
-              className="pb-2 min-w-36 bg-transparent rounded-md overflow-hidden"
-              key={player.id}
-            >
-              <div>
-                <Image
-                  src={`https://media.themoviedb.org/t/p/w138_and_h175_face${player.profile_path}`}
-                  alt={player.name}
-                  width={141}
-                  height={175}
-                  className="w-full"
-                />
-              </div>
-              <div className="p-3">
-                <p className="text-white">{player.name}</p>
-                <p className="text-white">{player.original_name}</p>
-              </div>
-            </li>
-          ))}
-        </ol>
-      </div> */}
     </div>
   );
 };
