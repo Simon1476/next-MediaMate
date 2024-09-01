04f7dfa2420c5e4d8b525e3149674dbcc7f3d12b
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-09-01 23:08:01 +0900

    feat: MovieDetailPage 개선 및 장르 리스트 컴포넌트 추가
    - MovieDetailPage에 새로운 GenresList 컴포넌트를 추가하여 장르 표시를 개선
    
    - 사용자의 세션 상태(sessionId)에 따라 좋아요 및 평가 버튼(LikeButton, RateButton)을 표시하도록 조건부 렌더링을 추가
    
    - 기존의 장르 표시 방식을 변경하여 GenresList 컴포넌트를 사용하도록 수정

--- app/(root)/movie/[id]/page.tsx
+++ app/(root)/movie/[id]/page.tsx
@@ -1,5 +1,8 @@
 import LikeButton from "@/components/buttons/LikeButton";
 import RateButton from "@/components/buttons/RateButton";
+import GenresList from "@/components/genre/GenresList";
+import { useAuth } from "@/context/AuthContext";
+import { getSessionId } from "@/lib/cookies";
 import { getMovieCredits, getMovieDetail } from "@/lib/tmdb";
 import { formatRuntime } from "@/lib/utils";
 import { CastMember } from "@/types/tmdb";
@@ -9,29 +12,32 @@ const MovieDetailPage = async ({ params }: { params: { id: string } }) => {
   const id = Number(params.id);
 
   const movieDetail = await getMovieDetail(id);
-  const genres = movieDetail.genres.map((genre) => genre);
-
   const movieCredits = await getMovieCredits(id);
 
   const movieActionPlayers: CastMember[] = movieCredits.cast.filter(
     (cast) => cast.known_for_department === "Acting"
   );
 
+  const sessionId = await getSessionId();
+
   return (
     <div className="">
-      <div className="flex gap-16">
-        <Image
-          src={`https://image.tmdb.org/t/p/w300${movieDetail.poster_path}`}
-          alt={movieDetail.title}
-          width={300}
-          height={428}
-          className="rounded-lg"
-        />
+      <div className="flex flex-col gap-16 md:flex-row">
+        <div className="self-center flex-grow md:flex-none">
+          <Image
+            src={`https://image.tmdb.org/t/p/w300${movieDetail.poster_path}`}
+            alt={`Poster of ${movieDetail.title}`}
+            width={300}
+            height={428}
+            className="rounded-lg"
+          />
+        </div>
+
         {/* 영화 상세 정보 */}
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col items-center gap-4 md:items-stretch">
           {/* 제목 */}
-          <h2 className="text-32 text-white font-bold">{movieDetail.title}</h2>
-          <p className="flex items-center text-white gap-2">
+          <h2 className="font-bold text-white text-32">{movieDetail.title}</h2>
+          <p className="flex items-center gap-2 text-white">
             <Image
               src="/icons/gold-star.svg"
               alt="Star icon"
@@ -39,7 +45,7 @@ const MovieDetailPage = async ({ params }: { params: { id: string } }) => {
               height={20}
             />
             {/* 평점 */}
-            <span className="text-20 font-semibold">
+            <span className="font-semibold text-20">
               {movieDetail.vote_average === 0
                 ? "No vote"
                 : movieDetail.vote_average}
@@ -53,26 +59,26 @@ const MovieDetailPage = async ({ params }: { params: { id: string } }) => {
 
           <p className="text-white">{movieDetail.overview}</p>
 
-          <div className="flex gap-2">
+          <div className="flex flex-wrap justify-center gap-2 md:flex-nowrap md:justify-normal">
             {/* 장르 */}
-            <span className="text-white font-medium min-w-24">장르</span>
-            {genres.map((genre) => (
-              <span className="text-white" key={genre.id}>
-                {genre.name}
-              </span>
-            ))}
+            <span className="font-medium text-center text-white min-w-24 md:text-left">
+              장르
+            </span>
+            <GenresList genres={movieDetail.genres} mediaType="movie" />
           </div>
           {/* 좋아요, 시청 목록, 평가 버튼 */}
-          <div className="flex">
-            <LikeButton mediaType="movie" mediaId={movieDetail.id} />
-            <RateButton movieId={movieDetail.id} />
-          </div>
+          {sessionId && (
+            <div className="space-x-2">
+              <LikeButton mediaType="movie" mediaId={movieDetail.id} />
+              <RateButton movieId={movieDetail.id} />
+            </div>
+          )}
         </div>
       </div>
       {/* 출연자 목록*/}
       <div>
-        <h3 className="text-20 text-white my-4">액션 배우</h3>
-        <ol className="overflow-x-scroll flex gap-3">
+        <h3 className="my-4 text-white text-20">액션 배우</h3>
+        <ol className="flex gap-3 overflow-x-scroll">
           {movieActionPlayers.map((player) => {
             const profilePath = player.profile_path
               ? `https://image.tmdb.org/t/p/w138_and_h175_face${player.profile_path}`
@@ -80,13 +86,13 @@ const MovieDetailPage = async ({ params }: { params: { id: string } }) => {
 
             return (
               <li
-                className="pb-2 min-w-36 bg-transparent rounded-md overflow-hidden"
+                className="pb-2 overflow-hidden bg-transparent rounded-md min-w-36"
                 key={player.id}
               >
                 <div className="h-[175px]">
                   <Image
                     src={profilePath}
-                    alt={player.name}
+                    alt={`Profile of ${player.name}`}
                     width={138}
                     height={175}
                   />
