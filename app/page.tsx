80649715be0d3b221633cfc96121d88c2d7cba5a
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-10-01 23:38:54 +0900

    feat: 홈페이지에 로맨틱 코미디 영화 리스트 추가

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,10 @@ const Home = async ({ params }: { params: { id: string } }) => {
     `https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=false&page=1&sort_by=popularity.desc&with_genres=27`
   );
 
+  const rocoMovieList = await getMovies(
+    `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko-KR&page=1&sort_by=popularity.desc&with_genres=35%2C%2010749`
+  );
+
   return (
     <main className=" bg-[#0f1114] min-h-[calc(100vh-74px)]">
       <section className="pt-32">
@@ -26,8 +30,8 @@ const Home = async ({ params }: { params: { id: string } }) => {
       </section>
       <section className="py-10 sm:py-22">
         <div className="px-4 mx-auto max-w-screen-2xl">
-          <h2 className="pb-3 text-2xl font-bold text-white">범죄 및 스릴러</h2>
-          <Carousel mediaList={horrorMovieList.results} />
+          <h2 className="pb-3 text-2xl font-bold text-white">로맨스 코미디</h2>
+          <Carousel mediaList={rocoMovieList.results} />
         </div>
       </section>
     </main>
