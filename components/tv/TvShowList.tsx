b0ca021e9d18a3ad88d9c6d491b59b4d1d1a659a
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-11-25 22:57:34 +0900

    feat: TvShowList 컴포넌트에 스켈레톤 UI 추가 및 페이지네이션 구현

--- components/tv/TvShowList.tsx
+++ components/tv/TvShowList.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import { TMDBTVShow } from "@/types/tmdb";
 import Pagination from "../pagination/Pagination";
 import TvShowCard from "./TvShowCard";
+import TvSkeleton from "../TvSkeleton";
+import { useState, useEffect } from "react";
+import Pagination2 from "../pagination/Pagination2";
 
 const TvShowList = ({
   tvShowLists,
@@ -11,14 +16,29 @@ const TvShowList = ({
   totalPage: number;
   currentPage: number;
 }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [tvShowLists, currentPage]);
+
   return (
     <>
-      <div className="grid gap-4 grid-movie-responsive">
-        {tvShowLists.map((tvShow) => (
-          <TvShowCard key={tvShow.id} tvShow={tvShow} />
-        ))}
-      </div>
-      <Pagination totalPage={totalPage} currentPage={currentPage} />
+      {isLoading ? (
+        <TvSkeleton />
+      ) : (
+        <div className="grid gap-4 grid-movie-responsive">
+          {tvShowLists.map((tvShow) => (
+            <TvShowCard key={tvShow.id} tvShow={tvShow} />
+          ))}
+        </div>
+      )}
+      <Pagination2 totalPages={totalPage} mediaType="tv" />
     </>
   );
 };
