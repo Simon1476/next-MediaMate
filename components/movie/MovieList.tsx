550594db61bbf07c1e5db35113beb760f7cebf77
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-11-25 22:39:14 +0900

    feat: MovieList 컴포넌트에 로딩 상태와 스켈레톤 컴포넌트를 추가

--- components/movie/MovieList.tsx
+++ components/movie/MovieList.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import { TMDBMovie } from "@/types/tmdb";
 import MovieCard from "./MovieCard";
 import Pagination from "../pagination/Pagination";
+import { useState, useEffect } from "react";
+import MovieSkeleton from "../MovieSkeleton";
 
 const MovieList = ({
   movieLists,
@@ -11,14 +15,28 @@ const MovieList = ({
   totalPage: number;
   currentPage: number;
 }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [movieLists, currentPage]);
+
   return (
     <>
-      {/* <div className="flex flex-wrap gap-8 justify-evenly xl:justify-between"> */}
-      <div className="grid gap-4 grid-movie-responsive">
-        {movieLists.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
-      </div>
+      {isLoading ? (
+        <MovieSkeleton />
+      ) : (
+        <div className="grid gap-4 grid-movie-responsive">
+          {movieLists.map((movie, index) => (
+            <MovieCard key={movie.id} movie={movie} priority={index === 0} />
+          ))}
+        </div>
+      )}
       <Pagination totalPage={totalPage} currentPage={currentPage} />
     </>
   );
