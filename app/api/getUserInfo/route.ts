e0de35f8b51c14b01496bf2c031667c67a803562
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-11-29 17:32:00 +0900

    reafctor: 하드코딩된 계정 ID를 쿠키의 동적 accountId로 교체

--- app/api/getUserInfo/route.ts
+++ app/api/getUserInfo/route.ts
@@ -4,10 +4,10 @@ import { cookies } from "next/headers";
 export async function GET(request: Request) {
   const cookieStore = cookies();
   const sessionId = cookieStore.get("session_id")?.value;
-
+  const accountId = cookieStore.get("accound_Id")?.value;
   try {
     const response = await fetch(
-      `https://api.themoviedb.org/3/account/21416335?session_id=${sessionId}`,
+      `https://api.themoviedb.org/3/account/${accountId}?session_id=${sessionId}`,
       {
         method: "GET",
         headers: {
@@ -26,17 +26,14 @@ export async function GET(request: Request) {
       secure: process.env.NODE_ENV === "production",
       path: "/",
     });
-
     return res;
   } catch (error) {
-    if (error instanceof Error) {
-      return NextResponse.json({ error: error.message }, { status: 500 });
-    } else {
-      // Error 인스턴스가 아닐 경우, 기본 메시지를 사용
-      return NextResponse.json(
-        { error: "An unknown error occurred" },
-        { status: 500 }
-      );
-    }
+    return NextResponse.json(
+      {
+        error:
+          error instanceof Error ? error.message : "An unknown error occurred",
+      },
+      { status: 500 }
+    );
   }
 }
