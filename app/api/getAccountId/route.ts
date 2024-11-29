e9ba3a619fb491a963f481fe559ffa6406601c92
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-11-29 17:28:39 +0900

    feat: 세션 쿠키를 사용하여 계정 ID를 가져오는 GET 엔드포인트 추가

---
+++ app/api/getAccountId/route.ts
@@ -0,0 +1,39 @@
+import { cookies } from "next/headers";
+import { type NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const cookieStore = cookies();
+  const sessionId = cookieStore.get("session_id");
+  try {
+    const accountResponse = await fetch(
+      `https://api.themoviedb.org/3/account?api_key=${process.env.TMDB_API_KEY}&session_id=${sessionId?.value}`
+    );
+
+    if (!accountResponse.ok) {
+      throw new Error(
+        `Failed to fetch account data: ${accountResponse.statusText}`
+      );
+    }
+
+    const accountData = await accountResponse.json();
+    const res = NextResponse.json(accountData);
+    console.log("accountData", accountData);
+    res.cookies.set("accound_Id", accountData.id, {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+      path: "/",
+    });
+    return res;
+  } catch (error) {
+    console.error("Error fetching account data:", error);
+    return NextResponse.json(
+      {
+        error:
+          error instanceof Error ? error.message : "An unknown error occurred",
+      },
+      { status: 500 }
+    );
+  }
+}
