093e0b1b774525ec66c15f5d6edced10ac46c1e5
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-12-09 21:48:47 +0900

    refactor: accountId 관리 로직 제거 및 AuthProvider 단순화

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,25 +1,20 @@
 "use client";
 
-import { deleteAccountId, deleteSessionId } from "@/lib/cookies";
+import { deleteSessionId } from "@/lib/cookies";
 import { AuthContextType } from "@/types";
 import { createContext, useContext, useEffect, useState } from "react";
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 const USERNAME_KEY = "username";
-const ACCOUNT_ID_KEY = "accountId";
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [username, setUserName] = useState<string | null>(null);
-  const [accountId, setAccountId] = useState<string | null>(null);
 
   useEffect(() => {
-    // 브라우저 환경에서만 실행되도록 보장
     const storedUsername = localStorage.getItem(USERNAME_KEY);
-    const storedAccountId = localStorage.getItem(ACCOUNT_ID_KEY);
 
     if (storedUsername) setUserName(storedUsername);
-    if (storedAccountId) setAccountId(storedAccountId);
   }, []);
 
   useEffect(() => {
@@ -31,15 +26,6 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     }
   }, [username]);
 
-  useEffect(() => {
-    // accountId 상태가 변경될 때 로컬 스토리지 업데이트
-    if (accountId) {
-      localStorage.setItem(ACCOUNT_ID_KEY, accountId);
-    } else {
-      localStorage.removeItem(ACCOUNT_ID_KEY);
-    }
-  }, [accountId]);
-
   const fetchUserInfo = async () => {
     try {
       const response = await fetch("/api/getUserInfo");
@@ -47,7 +33,6 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
       if (response.ok) {
         const data = await response.json();
         setUserName(data.username);
-        setAccountId(data.id);
       } else {
         console.error("Failed to fetch user info:", response.statusText);
       }
@@ -58,16 +43,11 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 
   const signOut = () => {
     setUserName(null);
-    setAccountId(null);
     deleteSessionId();
-    deleteAccountId();
     localStorage.removeItem(USERNAME_KEY);
-    localStorage.removeItem(ACCOUNT_ID_KEY);
   };
   return (
-    <AuthContext.Provider
-      value={{ username, fetchUserInfo, signOut, accountId }}
-    >
+    <AuthContext.Provider value={{ username, fetchUserInfo, signOut }}>
       {children}
     </AuthContext.Provider>
   );
